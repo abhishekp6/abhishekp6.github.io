d0e5c250d6254992572ab2f0c9ac382678766667
abhishekp6 <53012981+abhishekp6@users.noreply.github.com>
2023-01-24 00:35:08 +0530

    Intial structural commit. Add Home, Skill section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
+
+import Home from './Home/Home';
+import Projects from './Components/Projects/Projects';
+import Blogs from './Components/Blogs/Blogs';
+import Skills from './Components/Skills/Skills';
+import Contact from './Components/Contact/Contact';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/projects" element={<Projects />}/>
+        <Route path="/blogs" element={<Blogs />}/>
+        <Route path="/skills" element={<Skills />}/>
+        <Route path="/contact" element={<Contact />}/>
+        <Route />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
