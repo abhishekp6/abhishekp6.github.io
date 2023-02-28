1e950a76959858cef7f5bd78ce3167130a3ebd1f
abhishekp6 <53012981+abhishekp6@users.noreply.github.com>
2023-02-28 22:59:33 +0530

    Add org links

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,8 +3,12 @@ import './Home.css';
 import profile from '../assets/Profile.jpeg';
 import footer from '../assets/footerImg.jpeg'
 import Typewriter from "typewriter-effect";
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+
+    const navigate = useNavigate();
+
     return(
         <div>
             <div className="header">
@@ -33,16 +37,16 @@ const Home = () => {
                 Hello there, welcome to my space. 
 
                 I am a Software Engineer/Developer from India. I love developing/building/creating/improving/tinkering.
-                Currently I am building data products with awesome folks at Lumiq.
+                Currently I am building data products with awesome folks at <span className='textHighlight' onClick={() => { window.open("https://lumiq.ai/", "_blank") }} >Lumiq</span>.
                 <br></br>
                 <br></br>
-                I have completed my undergrad in Computer Science from Jaypee Institute of Information Technology, Noida, India, in 2021.
+                I have completed my undergrad in Computer Science from <span className='textHighlight' onClick={() => { window.open("https://www.jiit.ac.in/", "_blank") }} >Jaypee Institute of Information Technology</span>, Noida, India, in 2021.
                 <br></br>
                 <br></br>
                 I am a strong believer in building for humanity. I support cause of quality education to each. I am a supporter of environmental sustainability.
                 <br></br>
                 <br></br>
-                Feel free to connect with me through contact section. I am always excited to connect/learn/build with like minded folks.
+                Feel free to connect with me through <span className='textHighlight' onClick={() => {navigate("/contact")}} >contact</span> section. I am always excited to connect/learn/build with like minded folks.
 
             </div>
 
